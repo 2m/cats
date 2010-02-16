924474ea57e9f40b4704af2fa3c80d4e070481f6
MMartynas <MMartynas@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-02-16 13:53:34 +0000

    simple network tests

--- trunk/cats-pc/src/se/uu/it/cats/pc/Main.java
+++ trunk/cats-pc/src/se/uu/it/cats/pc/Main.java
@@ -1,8 +1,10 @@
 package se.uu.it.cats.pc;
 
-import lejos.pc.comm.NXTCommFactory;
-import lejos.pc.comm.NXTConnector;
-import lejos.pc.comm.NXTInfo;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.pc.comm.*;
 
 public class Main
 {
@@ -10,21 +12,85 @@ public class Main
 	{
 		System.out.println("I am the computer!!!");
 		
-		// search for any nxt
 		NXTConnector conn = new NXTConnector();
 		
-		System.out.println("Protocol NXTCommFactory.USB:");
-		NXTInfo[] info = conn.search(null, null, NXTCommFactory.USB);
-		for (NXTInfo i : info)
+		/*System.out.println("Protocol NXTCommFactory.BLUETOOTH:");
+		NXTInfo[] info = conn.search(null, null, NXTCommFactory.BLUETOOTH);
+		for (NXTInfo nxti : info)
 		{
-			System.out.println("Object:"+i+" name:"+i.name);
+			if (nxti.name.equals("cat1"))
+			{
+				connected = conn.connectTo(nxti, NXTComm.PACKET);
+			}
+		}*/
+		
+		NXTInfo nxt = new NXTInfo(NXTCommFactory.BLUETOOTH, "cat1", "00165302CC4E");
+		boolean connected = conn.connectTo(nxt, NXTComm.PACKET);
+		
+		if (!connected)
+		{
+			System.err.println("Failed to connect to any NXT");
+			System.exit(1);
 		}
 		
-		System.out.println("Protocol NXTCommFactory.BLUETOOTH:");
-		info = conn.search(null, null, NXTCommFactory.BLUETOOTH);
-		for (NXTInfo i : info)
+		DataOutputStream dos = conn.getDataOut();
+		DataInputStream dis = conn.getDataIn();
+		
+		long sum = 0;
+		long min = 9999;
+		int minId = -1;
+		long max = 0;
+		int maxId = -1;
+		
+		for (int i = 0; i < 100; i++)
 		{
-			System.out.println("Object:"+i+" name:"+i.name);
+			try
+			{
+				long t0 = System.currentTimeMillis();
+				for (int j = 0; j < 200; j++)
+					dos.writeByte(0x00);
+				dos.flush();
+				dis.readByte();
+				long t1 = System.currentTimeMillis();
+				
+				long delta = t1 - t0;
+				
+				sum += delta;
+				
+				if (delta > max)
+				{
+					max = delta;
+					maxId = i;
+				}
+				
+				if (delta < min)
+				{
+					min = delta;
+					minId = i;
+				}
+			}
+			catch (IOException ioe)
+			{
+				System.out.println("IO Exception writing bytes:");
+				System.out.println(ioe.getMessage());
+				break;
+			}
 		}
+		
+		try
+		{
+			dis.close();
+			dos.close();
+			conn.close();
+		}
+		catch (IOException ioe)
+		{
+			System.out.println("IOException closing connection:");
+			System.out.println(ioe.getMessage());
+		}
+		
+		System.out.println("Average time: "+(sum / 100)+"ms");
+		System.out.println("Average bw: "+(200 * 1024 / ((double)sum / 100) / 1000)+"kB/s");
+		System.out.println("Max time: "+max+"ms ("+maxId+"), min time: "+min+"ms("+minId+")");
 	}
 }
