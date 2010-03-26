8137d7dfd34c392444a13aa183a51a8d2330bf0f
nils.tornblom <nils.tornblom@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-03-26 23:32:06 +0000

    mainUKFmerge.m contains both the mouse tracking and the cat positioning problem (newest). The cats can move as well, but they are stationary currently. The cats' vision should be improved - i.e. directed towards the mouse for the most part

---
+++ simulation/UKFsim/fovCheckMerge.m
@@ -0,0 +1,30 @@
+function [Rm,actLandm,actCats]=fovCheckMerge(z,zm,fov,large,camA);
+    global Rfull;
+    global n;
+    global r;
+    global Rfullm;
+    global nm;
+    global rm;
+    global R;
+    
+    for i=1:nm
+        R{1,i}=Rfull{1,i};
+        R{1,i}(1:n,1:n)=large*eye(n);
+        actLandm{1,i}=[];
+        for j=1:n
+            if abs(z(j,i)-camA)<fov/2
+                R{1,i}(j,j)=r(1)^2;
+                actLandm{1,i}(end+1)=j;
+            end
+        end
+    end
+    
+    Rm=Rfullm;
+    Rm(1:nm,1:nm)=large*eye(nm);
+    actCats=[];
+    for i=1:nm
+        if abs(zm(i,1)-camA)<fov/2
+            Rm(i,i)=rm^2;
+            actCats(end+1)=i;
+        end
+    end
