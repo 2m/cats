77870e373c13052af26b6dceb5f7e118bf521b58
nils.tornblom <nils.tornblom@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-03-26 23:36:08 +0000

    mainUKFmerge.m contains both the mouse tracking and the cat positioning problem (newest). The cats can move as well, but they are stationary currently. The cats' vision should be improved - i.e. directed towards the mouse for the most part

---
+++ simulation/UKFsim/reinit.m
@@ -0,0 +1,19 @@
+function reinit(activeCats)
+%reinitializes when cats drop out
+global n;
+global cats;
+global R;
+global r;
+global catsFull;
+n=0;
+for i=1:length(activeCats)
+    if activeCats(i)==1
+        n=n+1;
+        catsTemp(n,:)=catsFull(i,:);
+    end
+end
+if n==0
+    catsTemp=[];
+end
+R = r^2*eye(n);
+cats=catsTemp;
