43fc13d3cd5cc0b8d5a4396d3f20badf41edaef6
EdvardZak@gmail.com <EdvardZak@gmail.com@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-15 16:54:40 +0000

    A skeleton for the ukf, to be continued :)

---
+++ simulation/particle_filter/GSim/src/GSim/IUnscentedKalmanFilter.java
@@ -0,0 +1,31 @@
+package GSim;
+
+/**
+ * The interface for the Unscented Kalman Filter
+ * @author Edvard
+ *
+ */
+public interface IUnscentedKalmanFilter 
+{
+	/**
+	 * Ukf, Unscented Kalman Filter for nonlinear dynamic systems
+	 * [x, P] = ukf(f,x,P,h,z,Q,R) returns state estimate, x and state covariance, P 
+	 * for nonlinear dynamic system (for simplicity, noises are assumed as additive):
+	 *            x_k+1 = f(x_k) + w_k
+	 *            z_k   = h(x_k) + v_k
+	 * where w ~ N(0,Q) meaning w is gaussian noise with covariance Q
+	 *       v ~ N(0,R) meaning v is gaussian noise with covariance R    
+	 * @param f: function handle for f(x)
+	 * @param x: "a priori" state estimate
+	 * @param P: "a priori" estimated state covariance
+	 * @param h: fanction handle for h(x)
+	 * @param z: current measurement
+	 * @param Q: process noise covariance
+	 * @param R: measurement noise covariance
+	 * @param ???
+	 * @return x: "a posteriori" state estimate and P: "a posteriori" state covariance
+	 */
+	float[] Ukf(float fstate, float x, float P, float hmeas,
+			float z, float Q,float R, float Kzero);
+
+}
