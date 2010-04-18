c8766e3f04f08250b4a1b3d384a1dd1fa3c623d2
nils.tornblom <nils.tornblom@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-18 14:54:33 +0000

    some updates to ukf (the sigmas method) , the filter is not yet working

--- simulation/particle_filter/GSim/src/GSim/Hmeas.java
+++ simulation/particle_filter/GSim/src/GSim/Hmeas.java
@@ -2,10 +2,19 @@ package GSim;
 
 import lejos.util.Matrix;
 
+/**
+ * The measurement function
+ * @author Edvard
+ */
 public class Hmeas implements IFunction{
 	
 	public Matrix eval(Matrix x){
-		return null; //TODO implement measurement function
+		//TODO implement real measurement function
+		
+		//Basic measurement function to test the ukf implementation
+		//x is a (1)x(1) Matrix
+		Matrix output = new Matrix(1,1,x.get(0,0));
+		return output; 
 	}
 }
 
