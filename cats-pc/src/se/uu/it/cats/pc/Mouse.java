f1913db6f9d2346c97331f3675cfd82c80ed75e8
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-19 17:06:27 +0000

    pc program to calculate and display mouse position using high school math

---
+++ cats-pc/src/se/uu/it/cats/pc/Mouse.java
@@ -0,0 +1,93 @@
+package se.uu.it.cats.pc;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Mouse extends javax.swing.JFrame implements Runnable
+{
+	public static final int SCREEN_WIDTH = 700;
+    public static final int SCREEN_HEIGHT = 900;
+	
+	public static final int WORLD_WIDTH = SCREEN_WIDTH - 7;
+    public static final int WORLD_HEIGHT = SCREEN_HEIGHT - 29;    
+    
+    public static final int BALL_SIZE = 30;
+	
+	public static float _angles[] = new float[2];
+	
+	public float[] calcMousePos(float theta_mouse,float theta_tower)
+	{                                        
+		float xPos_tower = 0.5f;
+		float r_mouse = (float) (- xPos_tower*Math.sin(theta_tower) /Math.sin(theta_mouse - theta_tower ));  
+		float[] pos = new float[2];                                                                          
+		pos[0]=(float) (r_mouse*Math.cos(theta_mouse));                                                      
+		pos[1]=(float) (r_mouse*Math.sin(theta_mouse));                                                      
+		return pos;                                                                                          
+                                                                                                             
+	}
+
+	public void run()
+	{
+		openWindow();
+		
+		while (true)
+		{
+			repaint();
+			
+			try
+			{
+				Thread.sleep(1);
+			}
+			catch (Exception ex)
+			{
+				
+			}
+		}
+	}
+	
+	private void openWindow()
+    {
+        setContentPane(new JPanel()
+        {
+	        protected void paintComponent(Graphics g)
+	        {
+	                super.paintComponent(g);
+	                paintActors(g);
+	        }
+        });
+        
+        setVisible(true);
+        setResizable(false);
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+	
+	private void paintActors(Graphics g)
+	{
+		float[] pos = calcMousePos(_angles[0], _angles[1]);
+		
+		int xCoord = (int)(pos[0] * 1000);
+		int yCoord = WORLD_HEIGHT - (int)(pos[1] * 1000);
+		
+		Logger.println("Angle1:"+(_angles[0]*180)/Math.PI+" angle2:"+(_angles[1]*180)/Math.PI+" pos[0]:"+pos[0]+" pos[1]:"+pos[1]+" xCoord:"+xCoord+" yCoord"+yCoord);
+		
+		// mouse
+		g.setColor(Color.red);
+		g.fillOval(xCoord - BALL_SIZE / 2, yCoord - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE);
+		
+		// cat1
+		g.setColor(Color.blue);
+		g.fillOval(-BALL_SIZE / 2, WORLD_HEIGHT - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE);
+		
+		// cat2
+		g.setColor(Color.blue);
+		g.fillOval(500 - BALL_SIZE / 2, WORLD_HEIGHT - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE);
+	}
+}
