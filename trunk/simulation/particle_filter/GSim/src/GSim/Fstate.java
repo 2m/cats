4c15b8d27a283a9a395372b522f29204a4cf6748
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-23 15:28:16 +0000

    The uscented kalman filter, ukf, should be working now, it has been tested for one iteration wihtout noise so it needs further testing

--- trunk/simulation/particle_filter/GSim/src/GSim/Fstate.java
+++ trunk/simulation/particle_filter/GSim/src/GSim/Fstate.java
@@ -12,23 +12,23 @@ public class Fstate implements IFunction{
 		
 		//TODO implement real state function
 		
+		return null; 
+	}
+	
+	/*
+	public Matrix eval(Matrix x){
 		//Basic state function to test the ukf implementation
-		//x is a (n)x(1) Matrix
-
-
-		
+		//x is a (n)x(1) Matrix	
 		Matrix output = new Matrix(x.getRowDimension(),x.getColumnDimension());
-		
 		//System.out.println("Debug: In Fstate.eval(), x:");
 		//UnscentedKalmanFilter.printM(x);
 		
 		output.set(0, 0, x.get(1,0) );
 		output.set(1, 0, x.get(2,0) );
-		output.set(2, 0, 0.05 * x.get(0,0) * (x.get(1,0) + x.get(2,0)) );
-		
+		output.set(2, 0, 0.05 * x.get(0,0) * (x.get(1,0) + x.get(2,0)) );	
 		//System.out.println("Debug: In Fstate.eval(), output:");
 		//UnscentedKalmanFilter.printM(output);
-
 		return output; 
 	}
+	*/
 }
--- trunk/simulation/particle_filter/GSim/src/GSim/Hmeas.java
+++ trunk/simulation/particle_filter/GSim/src/GSim/Hmeas.java
@@ -8,13 +8,18 @@ import lejos.util.Matrix;
  */
 public class Hmeas implements IFunction{
 	
+	//TODO implement real measurement function
+	
 	public Matrix eval(Matrix x){
-		//TODO implement real measurement function
-		
+		return null;
+	}
+	
+	/*
+	public Matrix eval(Matrix x){
 		//Basic measurement function to test the ukf implementation
 		//x is a (1)x(1) Matrix
 		Matrix output = new Matrix(1,1,x.get(0,0));
 		return output; 
-	}
+	}*/
 }
 
