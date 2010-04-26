f0bb3b98ea93b6ca57c0dd529d39b442160e1aba
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-26 17:48:16 +0000

    Skeleton for a billboard

---
+++ trunk/simulation/particle_filter/GSim/src/GSim/BillBoard.java
@@ -0,0 +1,20 @@
+package GSim;
+
+/**
+ * Skeleton for the networking 
+ * @author Fredrik Wahlberg
+ *
+ */
+public class BillBoard {
+	// TODO: implement simulation of billboard class
+	public BillBoard() {
+		
+	}
+	public float[] getMeanAndCoveriance() {
+		float [] ret = new float[3];
+		// ret = {m_x, m_y, m'_x, m'_y, xx, xy, yy, x'x', x'y', y'y'}
+		// TODO: Calculate weighted mean and covariance
+		return ret;
+	}
+
+}
