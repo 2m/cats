1e761ec4ed42b29ba1c6d7d77727ecf1a8dc3464
EdvardZak@gmail.com <EdvardZak@gmail.com@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-26 21:10:37 +0000

    Some updates to the state/measurement functions for ukf

--- simulation/particle_filter/GSim/src/GSim/FstateMouse.java
+++ simulation/particle_filter/GSim/src/GSim/FstateMouse.java
@@ -3,12 +3,12 @@ package GSim;
 import lejos.util.Matrix;
 
 /**
- * State function for the mouse's position
+ * State function for the (tracked) mouse's position
  * @author Edvard
  */
 public class FstateMouse implements IFunction{
 	
-	public Matrix eval(Matrix x){
+	public Matrix eval(Matrix xm){
 		/*Original matlab code:
 		function x_update = update(x)
 		F=eye(4);
@@ -19,29 +19,13 @@ public class FstateMouse implements IFunction{
 		*/
 		
 		//TODO test this function		
-		Matrix x_update = Matlab.zeros(x.getRowDimension(), x.getColumnDimension());
+		Matrix xm_updated = Matlab.zeros(xm.getRowDimension(), xm.getColumnDimension());
 		Matrix F = Matlab.eye(4);
 		//here dt=1 since speed is measured in m/samples, thus:
 		F.set(1-1,3-1, 1);
 		F.set(2-1,4-1, 1);
-		x_update=F.times(x);
-		return x_update; 
+		xm_updated=F.times(xm);
+		return xm_updated; 
 	}
 	
-	/*
-	public Matrix eval(Matrix x){
-		//Basic state function to test the ukf implementation
-		//x is a (n)x(1) Matrix	
-		Matrix output = new Matrix(x.getRowDimension(),x.getColumnDimension());
-		//System.out.println("Debug: In Fstate.eval(), x:");
-		//UnscentedKalmanFilter.printM(x);
-		
-		output.set(0, 0, x.get(1,0) );
-		output.set(1, 0, x.get(2,0) );
-		output.set(2, 0, 0.05 * x.get(0,0) * (x.get(1,0) + x.get(2,0)) );	
-		//System.out.println("Debug: In Fstate.eval(), output:");
-		//UnscentedKalmanFilter.printM(output);
-		return output; 
-	}
-	*/
 }
--- simulation/particle_filter/GSim/src/GSim/HmeasMouse.java
+++ simulation/particle_filter/GSim/src/GSim/HmeasMouse.java
@@ -3,15 +3,39 @@ package GSim;
 import lejos.util.Matrix;
 
 /**
- * Measurement function for the mouse's position
+ * Measurement function for the (tracked) mouse's position
  * @author Edvard
  */
 public class HmeasMouse implements IFunction{
 	
 	//TODO implement real measurement function
 	
-	public Matrix eval(Matrix x){
+	
+	
+	public Matrix eval(Matrix xm){
 		return null;
+		/*
+		function zm = measurem(xm)
+		%calculates the bearings from the cats to the mouse and adds measurement noise
+		global x; %where we think the cats are
+		%global nm;
+		nm=size(x,2);
+		zm=zeros(nm,1);
+		for i=1:nm
+		    %use either acos or asin
+		    if (xm(2,1)-x(2,i)>=0) %needed because of ambiguity in acos (and asin)
+		                             %can be simplified(?)
+		        zm(i,1)=acos((xm(1,1)-x(1,i)) /...
+		           sqrt((xm(1,1)-x(1,i))^2 + (xm(2,1)-x(2,i))^2));
+		    else
+		        zm(i,1)=2*pi-acos((xm(1,1)-x(1,i)) /...
+		            sqrt((xm(1,1)-x(1,i))^2 + (xm(2,1)-x(2,i))^2));
+		    end
+		    
+		    %Alternative: (somtimes less divergent, sometimes more)
+		    %z(i,1)=atan2(x(2,1)-cats(i,2),x(1,1)-cats(i,1));
+		end
+		*/
 	}
 	
 	/*
