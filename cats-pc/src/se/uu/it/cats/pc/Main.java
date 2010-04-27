7c05fe3447aa6282671a0988b220e75b73ff63a9
ibbe64 <ibbe64@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-27 23:24:05 +0000

    Changes: - New panel for viewing data from bluetooth - Data to file saving capability - Some changes in cat-panel - Box in view-menu for turning on/off the grid

--- cats-pc/src/se/uu/it/cats/pc/Main.java
+++ cats-pc/src/se/uu/it/cats/pc/Main.java
@@ -1,6 +1,7 @@
 package se.uu.it.cats.pc;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -8,6 +9,7 @@ import java.awt.event.KeyEvent;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -30,8 +32,9 @@ public class Main
 	private PanelBluetooth panelBluetooth;
 	private int arenaWidth = 400;
 	private int arenaHeight = 400;
-	private int windowWidth = 600;
-	private int windowHeight = 300;
+	private int windowWidth = 800;
+	private int windowHeight = 700;
+	private JCheckBoxMenuItem sgrid;
 	
 	public Main() {
 		
@@ -39,15 +42,36 @@ public class Main
 		printNewArea = new PrintArea(newArea, arenaWidth, arenaHeight);
 		dataPanel = new DataPanel(newArea, arenaHeight);
 		panelBluetooth = new PanelBluetooth(windowWidth);
-	  
-		JFrame frame = new JFrame("Arena");
+		
+		// Create the Frame-window
+		JFrame frame = new JFrame("Cats - Arena");
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		
+		//Create menu-bar
 		JMenuBar menubar = new JMenuBar();
 	    ImageIcon icon = new ImageIcon("close.png");
 		JMenu file = new JMenu("File");
+		JMenu view = new JMenu("View");
 		file.setMnemonic(KeyEvent.VK_F);
+		view.setMnemonic(KeyEvent.VK_V);
 
+		// Add grid-box to view-menu
+		sgrid = new JCheckBoxMenuItem("Show grid");
+		sgrid.setState(true);
+		sgrid.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                if (sgrid.getState() == false ) { //Worked with false for some reason
+                    sgrid.setState(false);
+                    printNewArea.showGrid(false);
+                } else {
+                    sgrid.setState(true);
+                    printNewArea.showGrid(true);
+                }
+            }
+        });
+		view.add(sgrid);
+		
+		//Add close-item to file-menu
 	    JMenuItem fileClose = new JMenuItem("Close", icon);
 	    fileClose.setMnemonic(KeyEvent.VK_C);
 	    fileClose.setToolTipText("Exit application");
@@ -56,12 +80,13 @@ public class Main
 				System.exit(0);
 			}
 		});
-
 	    file.add(fileClose);
 		menubar.add(file);
-
+		menubar.add(view);
+		
 		frame.setJMenuBar(menubar);	
 		
+		//Creation of panels
 		JPanel panelBackground = new JPanel();
 		JPanel panelData = new JPanel();
 		JPanel panelArena = new JPanel();
@@ -70,7 +95,9 @@ public class Main
 		//frame.getContentPane().add(newArea);
 		
 		panelBackground.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
+		panelData.setBorder(BorderFactory.createLineBorder(Color.gray));
+		panelArena.setBorder(BorderFactory.createLineBorder(Color.gray));
+		
 		JButton button1 = new JButton("Button Text");
 		JLabel label1 = new JLabel("Label Text");
 		
@@ -100,6 +127,7 @@ public class Main
 		f.show();
 		pack();*/
 	    frame.setVisible(true);
+	    frame.setSize(windowWidth, windowHeight);
 		
 	    //f.getContentPane().add(new IteratorUnderStrike());
 	    //f.setSize(850, 250);
