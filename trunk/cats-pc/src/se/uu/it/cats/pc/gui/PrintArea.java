a4f2d563b166723557d5026cd5277eadc8af6b34
ibbe64 <ibbe64@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-29 00:37:08 +0000

    Changes: -Added zoom-function -Skeleton for settings-panel

--- trunk/cats-pc/src/se/uu/it/cats/pc/gui/PrintArea.java
+++ trunk/cats-pc/src/se/uu/it/cats/pc/gui/PrintArea.java
@@ -1,12 +1,13 @@
 package se.uu.it.cats.pc.gui;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 import java.util.Hashtable;
 
-public class PrintArea extends JPanel{ // implements ActionListener
+public class PrintArea extends JPanel implements ChangeListener{ // implements ActionListener
   
 	private Area _newArea;
 	private Cat[] _cats;
@@ -15,8 +16,13 @@ public class PrintArea extends JPanel{ // implements ActionListener
 	
 	private int _areaHeight;
 	private int _areaWidth;
+	
 	private int _arenaHeight;
 	private int _arenaWidth;
+	private int arenaHeightFix;
+	private int arenaWidthFix;
+	
+	private int zk = 50; // zoom variable
 	
 	private int centFix_X;
 	private int centFix_Y;
@@ -33,18 +39,54 @@ public class PrintArea extends JPanel{ // implements ActionListener
 	final static BasicStroke solid = new BasicStroke();		
 	
 	final static int wBetDashedLines = 60;
+	
+	// Slidern
+	private JSlider scaleSlider;
+	static final int SCALE_MIN = 1;
+	static final int SCALE_MAX = 100;
+	static final int SCALE_INIT = 50;
+	private int SCALE_CURRENT = SCALE_INIT;
   
   public PrintArea(Area newArea, int areaHeight, int areaWidth) {
 	_newArea = newArea;
 	_areaHeight = areaHeight;
 	_areaWidth = areaWidth;
+	
 	_arenaHeight = _newArea.getArenaHeight();
     _arenaWidth = _newArea.getArenaWidth();
+
+	arenaHeightFix = _arenaHeight;
+	arenaWidthFix = _arenaWidth;
 	// Size of window
     setPreferredSize(new Dimension(_areaWidth,_areaHeight));
     
 	// Bakgrundsfärgen
     setBackground(Color.white);
+    
+    // Add a slider with purpose to change scale
+    scaleSlider = new JSlider(JSlider.HORIZONTAL, SCALE_MIN, SCALE_MAX, SCALE_INIT);
+    scaleSlider.setMinorTickSpacing(10);
+    scaleSlider.setMajorTickSpacing(50);
+    scaleSlider.setPaintTicks(true);
+    scaleSlider.setPaintLabels(true);
+    scaleSlider.setBackground(Color.white);
+    scaleSlider.addChangeListener(this);
+
+    // Change view of slider
+    Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+    JLabel zin = new JLabel("In");
+    JLabel zoom = new JLabel("Zoom");
+    JLabel zout = new JLabel("Out");
+    zin.setForeground(Color.black);
+    zoom.setForeground(Color.black);
+    zout.setForeground(Color.black);
+    labelTable.put( new Integer( 10 ), zout );
+    labelTable.put( new Integer( SCALE_MAX/2 ), zoom );
+    labelTable.put( new Integer( SCALE_MAX ), zin );
+
+    scaleSlider.setLabelTable( labelTable );
+
+    add(scaleSlider);
 
   }
 
@@ -79,8 +121,8 @@ public class PrintArea extends JPanel{ // implements ActionListener
 
 	// g.drawOval( (int) ((constant.posJord().getX()-constant.radieJord())/constant.skala()) , (int) ((constant.posJord().getY()-constant.radieJord())/constant.skala()), (int) (2*constant.radieJord()/constant.skala()), (int) (2*constant.radieJord()/constant.skala()));
 
-	centFix_X = _areaWidth/2-_arenaWidth/2;
-	centFix_Y = _areaHeight/2+_arenaHeight/2;
+	centFix_X = _areaWidth/2-_arenaWidth/2*zk/50;
+	centFix_Y = _areaHeight/2+_arenaHeight/2*zk/50;
 	
 	//Print cats
     int size = Math.min(getWidth(), getHeight());
@@ -102,8 +144,8 @@ public class PrintArea extends JPanel{ // implements ActionListener
 			g2d.setColor(Color.black); // Black cats
 			
 			//Draw the cats
-			entityPosX = centFix_X+_cats[i].getX();
-			entityPosY = centFix_Y-_cats[i].getY();
+			entityPosX = centFix_X+_cats[i].getX()*zk/50;
+			entityPosY = centFix_Y-_cats[i].getY()*zk/50;
 			
 			g2d.fillOval( (int) entityPosX-5, (int) entityPosY-5, 10, 10);
 			//Streckat test
@@ -118,7 +160,7 @@ public class PrintArea extends JPanel{ // implements ActionListener
 		if (_lighthouse[i] != null) {
 			g2d.setColor(Color.green); 
 			if(i==3) { g2d.setColor(Color.blue); }
-			g2d.fillOval( (int) centFix_X+_lighthouse[i].getX()-15, (int) centFix_Y-_lighthouse[i].getY()-15, 30, 30);
+			g2d.fillOval( (int) centFix_X+(_lighthouse[i].getX()*zk/50-15), (int) centFix_Y-(_lighthouse[i].getY()*zk/50+15), 30, 30);
 		}
     }
 	
@@ -129,5 +171,14 @@ public class PrintArea extends JPanel{ // implements ActionListener
   
   public void showGrid(boolean showgrid){
 	  _showgrid = showgrid;
+  }
+  
+  public void stateChanged(ChangeEvent ce) {
+	  if (ce.getSource() == scaleSlider) {
+	      SCALE_CURRENT = scaleSlider.getValue();
+	      //_arenaHeight = (int) arenaHeightFix*SCALE_CURRENT/50;
+	      zk = SCALE_CURRENT;
+	      System.out.println(SCALE_CURRENT);
+	  }
   }
 };
