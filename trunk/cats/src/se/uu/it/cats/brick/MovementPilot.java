792a11c57ace026311a67e4c94341765b5548858
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-04-29 16:20:54 +0000

    Experimenting with MovementPilot

--- trunk/cats/src/se/uu/it/cats/brick/MovementPilot.java
+++ trunk/cats/src/se/uu/it/cats/brick/MovementPilot.java
@@ -1,22 +1,38 @@
 package se.uu.it.cats.brick;
 
+import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.robotics.navigation.Pilot;
 import lejos.robotics.navigation.TachoPilot;
 
 public class MovementPilot {	
-	public MovementPilot(float x, float y)
+	
+	//Instance variables
+	public Pilot pilot;
+	private float[] c_state;
+	
+	public MovementPilot(float[] c_state)
 	{
-		/*float[] c_state = filter.getState();
-		float deltaX = x-c_state[1];
-		float deltaY = y-c_state[2];
+		this.c_state = c_state;
+		
+		Motor.B.regulateSpeed(true);
+		Motor.C.regulateSpeed(true);
+		Motor.B.smoothAcceleration(true);
+		Motor.C.smoothAcceleration(true);
+			
+		pilot = new TachoPilot(0.054725f,0.0544862f, 0.167f,Motor.C, Motor.B, false); //0.05475f //old design cat1
+		//pilot = new TachoPilot(0.055f, 0.172f, Motor.B, Motor.C, true); //new design
+	    }	 
+	
+	public void travel(float x, float y){
+		float deltaX = x-c_state[0];
+		float deltaY = y-c_state[1];
 		float r = (float) Math.sqrt(Math.pow(deltaX,2)+Math.pow(deltaY,2));
 		float newAngle = (float) Math.acos(x/r);
-		float angle = (c_state[3]-newAngle);
-		Pilot pilot = new TachoPilot(0.055f, 0.16f, Motor.B, Motor.C, true);
-		pilot.setMoveSpeed(10);
+		float angle = (float) ((newAngle-c_state[2])*180/Math.PI);
+		pilot.setMoveSpeed(pilot.getMoveMaxSpeed()/2);
+		pilot.setTurnSpeed(pilot.getTurnMaxSpeed()/2);
 		pilot.rotate(angle);
-		pilot.travel(r);*/
-	   
-	    }	 
+		pilot.travel(r);
+	}
 }
