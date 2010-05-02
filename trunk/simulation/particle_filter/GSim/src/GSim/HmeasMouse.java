0af860c74a86bbc994d76970c98bf8a5870d491f
EdvardZak@gmail.com <EdvardZak@gmail.com@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-02 20:30:32 +0000

    Added the measurement function for the mouse and started to write the absolute positioning for the cats using the ukf class

--- trunk/simulation/particle_filter/GSim/src/GSim/HmeasMouse.java
+++ trunk/simulation/particle_filter/GSim/src/GSim/HmeasMouse.java
@@ -3,20 +3,52 @@ package GSim;
 import lejos.util.Matrix;
 
 /**
- * Measurement function for the (tracked) mouse's position
+ * Measurement function for the (tracked) mouse's position.
+ * Calculates the bearings from the cats to the mouse
  * @author Edvard
  */
 public class HmeasMouse implements IFunction{
 	
-	//TODO implement real measurement function
+	//Instance variables
+	/** (a reference to the) estimated state (position) of the cats.*/
+	private Matrix x;
+	/** number of cats, ie number of columns in x.*/
+	private int nm;
 	
-	
-	
+	/**
+	 * Constructor needed to be used for this class to work properly
+	 * @param x  a reference to the object containing the estimated state (position) of the cats
+	 */
+	public HmeasMouse(Matrix x){
+		this.x=x;
+		nm = x.getColumnDimension();
+	}
+
 	public Matrix eval(Matrix xm){
-		return null;
-		/*
+		//TODO test measurement function
+		//NB: All -1 in the indices are used to indicate the shift form the first array index in matlab = 1 to java's = 0.
+		Matrix zm = Matlab.zeros(nm, 1);
+		for (int i = 1; i<nm; i++)
+		{
+			//use either acos or asin
+			if (xm.get(2-1,1-1)-x.get(2-1,i-1)>=0) //needed because of ambiguity in acos (and asin)
+			{
+				zm.set(i-1,1-1,  Math.acos( xm.get(1-1,1-1)-x.get(1-1,i-1) ) / Math.sqrt( Math.pow(xm.get(1-1,1-1)-x.get(1-1,i-1),2) ) + Math.pow(xm.get(2-1,1-1)-x.get(2-1,i-1),2)  );
+
+			}
+			else
+			{
+				zm.set(i-1,1-1,  2*Math.PI-Math.acos( xm.get(1-1,1-1)-x.get(1-1,i-1) ) / Math.sqrt( Math.pow(xm.get(1-1,1-1)-x.get(1-1,i-1),2) ) + Math.pow(xm.get(2-1,1-1)-x.get(2-1,i-1),2)  );
+
+			}
+		}	
+		return zm;
+		
+		
+		/*Original matlab code::
 		function zm = measurem(xm)
-		%calculates the bearings from the cats to the mouse and adds measurement noise
+		%calculates the bearings from the cats to the mouse
+		%xm is where the cats think the mouse is
 		global x; %where we think the cats are
 		%global nm;
 		nm=size(x,2);
@@ -37,13 +69,5 @@ public class HmeasMouse implements IFunction{
 		end
 		*/
 	}
-	
-	/*
-	public Matrix eval(Matrix x){
-		//Basic measurement function to test the ukf implementation
-		//x is a (1)x(1) Matrix
-		Matrix output = new Matrix(1,1,x.get(0,0));
-		return output; 
-	}*/
 }
 
