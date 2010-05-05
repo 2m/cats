bf10c25036dcc79600803c31f9b1705d75473554
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-05 17:58:12 +0000

    missing files from last commit (?)

---
+++ trunk/simulation/particle_filter/GSim/src/GSim/TrackingFilter.java
@@ -0,0 +1,71 @@
+package GSim;
+
+import java.awt.Graphics;
+
+/**
+ * Base class for the absolute positioning filter (graphics code should be
+ * talken out befor use on the NXT)
+ */
+public abstract class TrackingFilter {
+	/** Id number */
+	protected int id;
+	/** Buffers with data on mouse sightings */
+	protected final Buffer sensorData;
+	/** Period of filter */
+	protected final float T;
+	/** Period of filter in milliseconds */
+	protected final int Tint;
+	/** Pointer to common clock object */
+	protected final RealTimeClock rttime;
+	/** Shared data object */
+	protected BillBoard billboard;
+
+	public TrackingFilter(int id, float T, Buffer sensorData,
+			RealTimeClock rttime, BillBoard billboard) {
+		/** Save id number */
+		this.id = id;
+		/** Period of filter */
+		this.T = T;
+		this.Tint = (int) (T * 1000);
+		/** Sorded buffer with sensor readings */
+		this.sensorData = sensorData;
+		/** Real time clock */
+		this.rttime = rttime;
+		/** Shared data */
+		this.billboard = billboard;
+	}
+
+	/** Poll estimated x position value from filter */
+	public float getX() {
+		return (float) 0.0;
+	}
+
+	/** Poll estimated y position value from filter */
+	public float getY() {
+		return (float) 0.0;
+	}
+
+	/** Poll estimated x velocity value from filter */
+	public float getXv() {
+		return (float) 0.0;
+	}
+
+	/** Poll estimated y velocity value from filter */
+	public float getYv() {
+		return (float) 0.0;
+	}
+
+	/**
+	 * Runs one update of the filter then exits
+	 */
+	public void update() {
+	}
+
+	/** Draw stuff, should not be included in final code on the NXT */
+	public void draw(Graphics g) {
+	}
+
+	/** Reset filter with some initial data */
+	public void initData(float x, float y, float xv, float yv) {
+	}
+}
