fe8ec5804eff921c9cff48b511b9da1b7fef4744
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-06 16:31:42 +0000

    Not tested but most likely :D working ukf based positioning filter. To be continued...

--- simulation/particle_filter/GSim/src/GSim/AbsolutePositioningUKF.java
+++ simulation/particle_filter/GSim/src/GSim/AbsolutePositioningUKF.java
@@ -18,12 +18,12 @@ public class AbsolutePositioningUKF extends AbsolutePositioningFilter
 	/** nonlinear state equations */
 	private IFunction f;
 	
-	/** estimated state vector, (1)x(n) Matrix, of the cat 
-	 * x, y, vx, vy, orientation, absCamAngle*/
+	/** estimated state vector, (n)x(1) Matrix, of the cat 
+	 * x, y, vx, vy, orientation, (absCamAngle)*/
 	private Matrix xc;
 	
-	/** true state of the cat (simulation only)*/
-	private Matrix sc;
+	///** true state of the cat (basic simulation only)*/
+	//private Matrix sc;
 	
 	/** state covariance */
 	private Matrix P;
@@ -39,6 +39,9 @@ public class AbsolutePositioningUKF extends AbsolutePositioningFilter
 	
 	/** covariance of measurement of the cat */
 	private Matrix R;
+	
+	/** Varible for time */
+	private int lastCurrentTime, currentTime;
 
 	
 	public AbsolutePositioningUKF(float T, Buffer sensorData, Buffer movementData, RealTimeClock rttime)		
@@ -50,8 +53,8 @@ public class AbsolutePositioningUKF extends AbsolutePositioningFilter
 		//LandmarkList, true positions of the landmarks are in this static class. HmeasCat accesses the landmark list directly
 		int n = 1;//TODO use the following: LandmarkList.landmarkX.length;  //number of landmarks
 		//int nm=1;  //number of cats, should always be one for a single positioning filter
-		int nz=n+4;  //?? number of elements in the measurement vector of the cats = number of landmarks + 4
-		int nx=6;  //number of variables in the cats' state vector
+		int nz=n+3;  //TODO: change back to nz=n+4  //number of elements in the measurement vector of the cats = number of landmarks + 4
+		int nx=5;  //TODO: change back to nx=6 //number of variables in the cats' state vector
 		
 		ufk_filter = new UnscentedKalmanFilter(nx,nz);
 	
@@ -82,7 +85,7 @@ public class AbsolutePositioningUKF extends AbsolutePositioningFilter
 		
 		P = eye(nz).timesEquals( pow(10,-3) );  //initial state covariance
 
-		sc = zeros(nx,1);  //initial true state of the cat (simulation only)
+		//sc = zeros(nx,1);  //initial true state of the cat (basic simulation only)
 		xc = zeros(nx,1);  //initial estimated state
 		/*
 		double[][] temp_s = {{0.0}, {0.0}, {1.0}};;  //initial state of the cats  TODO get initial state from buffer?
@@ -108,58 +111,90 @@ public class AbsolutePositioningUKF extends AbsolutePositioningFilter
 	 *            Initial angle
 	 */
 	public void initData(float x, float y, float angle) {
-		/*
-		// Set means for x, y and angle
-		mean_x = x;
-		mean_y = y;
-		mean_angle = angle)
-		// Set (co-)variance variables
-		// std => 5cm
-		varXX = Fixed.floatToFixed(0.0025);
-		varYY = Fixed.floatToFixed(0.0025);
-		// No co-variance
-		varXY = Fixed.floatToFixed(0.0);
-		// std => 3 degrees
-		varAngle = Fixed.floatToFixed(Math.pow(3 * (Math.PI / 180), 2));
-		*/
+		lastCurrentTime = rttime.getTime();
+		xc.set(0, 0, x);
+		xc.set(1, 0, y);	
+		xc.set(2, 0, 0);
+		xc.set(3, 0, 0);	
+		xc.set(4, 0, angle);	
 	}
 	
-	/*
-	public Matrix[] update(Matrix xc)
-	{
-		//TODO how to get input readings ???
-		//One iteration with UKF
-		z = h.eval(sc);  //h.eval(s).plus( Matrix.random(1,1).times(r) );  //measurments		
-		Matrix[] result = ufk_filter.ukf(f, xc, P, h, z, Q, R);
-		xc = result[0];  
-		P = result[1];
-		sc = f.eval(sc);  //update process 
-		
-		return result;
-	}*/
 	
 	/**
 	 * Updates the filter without explicitly returning the values
 	 */
-	public void update()
-	{
-		//One iteration with UKF
-		//z = h.eval(sc);  //h.eval(s).plus( Matrix.random(1,1).times(r) );  //measurments	
+	public void update() {
+		// Get time reference
+		currentTime = rttime.getTime();
+
+		// Update landmark angle in the measurement matrix
+		SightingData sdata = (SightingData) sensorData.pop();
+		while (sdata != null) 
+		{
+			if (sdata.getComparable() <= currentTime) 
+			{
+				//Determine which landmark it is
+				double absLandmarkAngle =  sdata.angle;
+				if (absLandmarkAngle>=0 && absLandmarkAngle<90) //upper right corner
+				{
+					z.set(3,0,sdata.angle);
+				}
+				else if (absLandmarkAngle>=90 && absLandmarkAngle<180) //upper left corner
+				{
+					z.set(1,0,sdata.angle);
+				}
+				else if (absLandmarkAngle>=180 && absLandmarkAngle<270) //lower left corner
+				{
+					z.set(0,0,sdata.angle);
+				}
+				else if (absLandmarkAngle>=270 && absLandmarkAngle<360)  //lower right corner
+				{
+					z.set(2,0,sdata.angle);
+				}
+				else System.out.print("ERROR in update!");
+			} 
+			else 
+			{
+				sensorData.push(sdata);
+				sdata = null;
+			}
+		}
 		
-		MovementData mdata = (MovementData) movementData.pop();
-		SightingData sdata = (SightingData) sensorData.pop();//TODO is sighting data only one landmark ??
-		double[] bearingToLandmarks;
-		//TODO: fovCheckMerge...
-		//double tempX = getX() + mdata. //TODO add tacometer data
-		//double tempY = getY() + mdata.
-		double tempOrientation = xc.get(0, 4) + mdata.dangle;
-		z.set(0,4,tempOrientation);
-	
+		// Update cat velocity and orientation in the measurement matrix
+		MovementData mdata = (MovementData) movementData.pop();	
+		double xPositionFromTachometer = xc.get(0, 0);
+		double yPositionFromTachometer = xc.get(1, 0);
+		double orientationFromTachometer = xc.get(4, 0);
+		while (mdata != null) 
+		{
+			if (mdata.getComparable() <= currentTime) 
+			{
+				double distance = mdata.dr;
+				xPositionFromTachometer += cos(distance);
+				yPositionFromTachometer += sin(distance);
+				orientationFromTachometer += mdata.dangle;
+				
+				mdata = (MovementData) movementData.pop();
+			}
+			else {
+				movementData.push(mdata);
+				mdata = null;
+			}
+		}
+		double xVelocityFromTachometer = (z.get(0, 0) - xPositionFromTachometer) / (lastCurrentTime - currentTime);
+		double yVelocityFromTachometer = (z.get(1, 0) - yPositionFromTachometer) / (lastCurrentTime - currentTime);
+		z.set(2, 0, xVelocityFromTachometer);
+		z.set(3, 0, yVelocityFromTachometer);	
+		z.set(4, 0, orientationFromTachometer);	
+		
+		//One iteration with UKF
 		Matrix[] result = ufk_filter.ukf(f, xc, P, h, z, Q, R);
 		xc = result[0];  
 		P = result[1];
-		//sc = f.eval(sc);  //update process, TODO: Remove ? Don't know true state ?
-	}
+		//sc = f.eval(sc);  //update process (basic simulation only)
+		
+		lastCurrentTime = currentTime;	
+	}//end of update
 	
 	/** Poll estimated x position value from filter */
 	public float getX() {
