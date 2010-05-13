60511cc67489a0dfb3e6724b021701b01ced49f7
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-13 16:34:39 +0000

    by MMartynas, BillBoard fix. Now uses timestamp to discard late packets (will probably never happen)

--- trunk/cats/src/se/uu/it/cats/brick/storage/BillBoard.java
+++ trunk/cats/src/se/uu/it/cats/brick/storage/BillBoard.java
@@ -1,5 +1,6 @@
 package se.uu.it.cats.brick.storage;
 
+import se.uu.it.cats.brick.Clock;
 import se.uu.it.cats.brick.network.ConnectionManager;
 import se.uu.it.cats.brick.network.packet.LatestSightingUpdate;
 import se.uu.it.cats.brick.network.packet.MeanAndCovarianceUpdate;
@@ -9,6 +10,9 @@ public class BillBoard
 	/** Data to be synced */
 	private float[][] data;
 	private float[] sightings;
+	
+	private int latestDataUpdate = 0;
+	private int latestSightingUpdate = 0;
 
 	/** Floats to sync per cat */
 	private final int DATA_PER_CAT = 11;
@@ -47,8 +51,13 @@ public class BillBoard
 	public void setLatestSighting(LatestSightingUpdate p) {
 		// id in range [0:n-1]
 		// got data from other device,
-		// save data and do not send an update
-		setLatestSighting(p.getSource(), p.getX(), p.getY(), p.getTheta(), false);
+		// check if it is new
+		if (p.getTimestamp() > latestSightingUpdate)		
+		{
+			// save data and do not send an update
+			setLatestSighting(p.getSource(), p.getX(), p.getY(), p.getTheta(), false);
+			latestDataUpdate = p.getTimestamp();
+		}
 	}
 
 	public void setLatestSighting(int id, float x, float y, float theta, boolean sendUpdate) {
@@ -59,7 +68,7 @@ public class BillBoard
 		if (sendUpdate)
 			// send the update to all
 			ConnectionManager.getInstance().sendPacketToAll(
-					new LatestSightingUpdate(x, y, theta)
+					new LatestSightingUpdate(x, y, theta, Clock.timestamp())
 			);
 	}
 	
@@ -81,21 +90,25 @@ public class BillBoard
 	
 	public void setMeanAndCovariance(MeanAndCovarianceUpdate p) {
 		// id in range [0:n-1]
-		setMeanAndCovariance(
-				p.getSource(),
-				p.getMeanX(),
-				p.getMeanY(),
-				p.getMeanXv(),
-				p.getMeanYv(),
-				p.getVarXX(),
-				p.getVarXY(),
-				p.getVarYY(),
-				p.getVarXvXv(),
-				p.getVarXvYv(),
-				p.getVarYvYv(),
-				p.getWeight(),
-				false
-		);
+		if (p.getTimestamp() > latestDataUpdate)
+		{
+			setMeanAndCovariance(
+					p.getSource(),
+					p.getMeanX(),
+					p.getMeanY(),
+					p.getMeanXv(),
+					p.getMeanYv(),
+					p.getVarXX(),
+					p.getVarXY(),
+					p.getVarYY(),
+					p.getVarXvXv(),
+					p.getVarXvYv(),
+					p.getVarYvYv(),
+					p.getWeight(),
+					false
+			);
+			latestDataUpdate = p.getTimestamp();
+		}
 	}
 	
 	/** Set mean and covariance */
@@ -120,7 +133,7 @@ public class BillBoard
 			// send the update to all
 			ConnectionManager.getInstance().sendPacketToAll(
 					new MeanAndCovarianceUpdate(mean_x, mean_y, mean_xv, mean_yv,
-							var_xx, var_xy, var_yy, var_xvxv, var_xvyv, var_yvyv, weight)
+							var_xx, var_xy, var_yy, var_xvxv, var_xvyv, var_yvyv, weight, Clock.timestamp())
 			);
 	}
 
