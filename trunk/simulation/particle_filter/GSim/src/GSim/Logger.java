9846df9c9a9e19eae08a7b1badcb078423a0e431
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-17 14:31:53 +0000

    Copy of Logger class from the code of the project cats. Adjusted so that it works with GSim.

---
+++ trunk/simulation/particle_filter/GSim/src/GSim/Logger.java
@@ -0,0 +1,22 @@
+package GSim;
+
+public class Logger {
+	public static void init() {
+	}
+
+	public static void print(String msg) {
+		System.out.print(msg);
+	}
+
+	public static void println(String msg) {
+		System.out.println(msg);
+	}
+
+	public static void print(int i) {
+		System.out.print(i);
+	}
+
+	public static void println(int i) {
+		System.out.println(i);
+	}
+}
