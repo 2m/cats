690ae117de3ee2e36315b8cc40ca3896b24ec562
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-19 14:32:06 +0000

    Imported from GSim

--- trunk/cats/src/se/uu/it/cats/brick/filter/MovementData.java
+++ trunk/cats/src/se/uu/it/cats/brick/filter/MovementData.java
@@ -3,11 +3,22 @@ package se.uu.it.cats.brick.filter;
 public class MovementData extends ComparableData {
 	public float dr; // Distance travelled
 	public float dangle; // Radians turned
+	public float xv;
+	public float yv;
 
 	public MovementData(int timestamp, float dr, float dangle) {
 		super(timestamp);
 		this.dr = dr;
 		this.dangle = dangle;
+	}
+
+	public MovementData(int timestamp, float dr, float dangle, float xv,
+			float yv) {
+		super(timestamp);
+		this.dr = dr;
+		this.dangle = dangle;
+		this.xv = xv;
+		this.yv = yv;
 	}
 
 	public boolean isMovementData() {
