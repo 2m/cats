03abbf7d3e8c2ad0800d07c3f8f26ce17b94aa67
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-19 14:45:39 +0000

    Small fix

--- trunk/simulation/particle_filter/GSim/src/GSim/SensorHandler.java
+++ trunk/simulation/particle_filter/GSim/src/GSim/SensorHandler.java
@@ -31,7 +31,7 @@ public class SensorHandler {
 		float cy = (float) cat.getY();
 		float x1 = (float) mouse.getX() - cx;
 		float y1 = (float) mouse.getY() - cy;
-		float norm1 = (float) Math.sqrt(x1 * x1 + y1 * y1);
+		// float norm1 = (float) Math.sqrt(x1 * x1 + y1 * y1);
 		float angle1 = (float) Math.atan2(y1, x1);
 		int t = Clock.getTime();
 		int type = 0;
@@ -44,7 +44,7 @@ public class SensorHandler {
 		for (int i = 0; i < LandmarkList.landmarkX.length; i++) {
 			float x2 = LandmarkList.landmarkX[i] - cx;
 			float y2 = LandmarkList.landmarkY[i] - cy;
-			float norm2 = (float) Math.sqrt(x2 * x2 + y2 * y2);
+			// float norm2 = (float) Math.sqrt(x2 * x2 + y2 * y2);
 			float angle2 = (float) Math.atan2(y2, x2);
 			if (LandmarkList.landmarkC[i]) {
 				type = LandmarkList.GREEN;
@@ -53,12 +53,15 @@ public class SensorHandler {
 			}
 			// System.out.println("(cx, cy, angle)=(" + cx + ", " + cy + ", " +
 			// angle2*(180/Math.PI) + ")");
-			//float angle_diff = (float) Math.acos((x1 * x2 + y1 * y2)
-			//		/ (norm1 * norm2));
-			float angle_diff = (float)Math.abs(angle2-cat.getAngle());
-			//System.out.println("In sensor, landmark " + i + ", angle diff = " + Math.toDegrees(angle_diff) + ", fov/2 = " +  Math.toDegrees((field_of_view / 2)));
+			// float angle_diff = (float) Math.acos((x1 * x2 + y1 * y2)
+			// / (norm1 * norm2));
+			float angle_diff = (float) Math.abs(angle2 - cat.getAngle());
+			// System.out.println("In sensor, landmark " + i + ", angle diff = "
+			// + Math.toDegrees(angle_diff) + ", fov/2 = " +
+			// Math.toDegrees((field_of_view / 2)));
 			if ((initialPhase) || (angle_diff < (field_of_view / 2))) {
-				//System.out.println("In sensor, landmark " + i + " within field of view");
+				// System.out.println("In sensor, landmark " + i +
+				// " within field of view");
 				/*
 				 * d = new SightingData(t, cx, cy, (float) ((angle2 -
 				 * cat.getAngle()) + rnd.nextGaussian() 3 * (Math.PI / 180)),
@@ -73,10 +76,11 @@ public class SensorHandler {
 				positioningBuffer.push(d);
 			}
 		}
-		
-		//Needs to leave initial phase to make the field of view check work
-		if (initialPhase) initialPhase = false;
-		
+
+		// Needs to leave initial phase to make the field of view check work
+		if (initialPhase)
+			initialPhase = false;
+
 		// TODO: Check if initial convergence phase has ended
 	}
 
