292088b8b41dffdf80ffe2aa3b1251df726637f9
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-19 15:16:11 +0000

    Small fixes

--- trunk/cats/src/se/uu/it/cats/brick/filter/TrackingFilter.java
+++ trunk/cats/src/se/uu/it/cats/brick/filter/TrackingFilter.java
@@ -1,12 +1,12 @@
 package se.uu.it.cats.brick.filter;
 
-import java.awt.Graphics;
+import se.uu.it.cats.brick.storage.BillBoard;
 
 /**
- * Base class for the absolute positioning filter (graphics code should be
- * talken out befor use on the NXT)
+ * Base class for the absolute positioning filter (graphics code should be taken
+ * out before use on the NXT)
  */
-public abstract class TrackingFilter extends Thread{
+public abstract class TrackingFilter extends Thread {
 	/** Id number */
 	protected int id;
 	/** Buffers with data on mouse sightings */
@@ -15,13 +15,11 @@ public abstract class TrackingFilter extends Thread{
 	protected final float T;
 	/** Period of filter in milliseconds */
 	protected final int Tint;
-	/** Pointer to common clock object */
-	protected final RealTimeClock rttime;
 	/** Shared data object */
 	protected BillBoard billboard;
 
 	public TrackingFilter(int id, float T, Buffer sensorData,
-			RealTimeClock rttime, BillBoard billboard) {
+			BillBoard billboard) {
 		/** Save id number */
 		this.id = id;
 		/** Period of filter */
@@ -29,13 +27,11 @@ public abstract class TrackingFilter extends Thread{
 		this.Tint = (int) (T * 1000);
 		/** Sorded buffer with sensor readings */
 		this.sensorData = sensorData;
-		/** Real time clock */
-		this.rttime = rttime;
 		/** Shared data */
 		this.billboard = billboard;
 		// Set priority for thread
 		// TODO: Decide priority for absolute positioning filter
-		setPriority(Thread.MIN_PRIORITY); 
+		setPriority(Thread.MIN_PRIORITY);
 	}
 
 	/** Poll estimated x position value from filter */
@@ -64,13 +60,10 @@ public abstract class TrackingFilter extends Thread{
 	public void update() {
 	}
 
-	/** Draw stuff, should not be included in final code on the NXT */
-	public void draw(Graphics g) {
-	}
-
 	/** Reset filter with some initial data */
 	public void initData(float x, float y, float xv, float yv) {
 	}
+
 	/**
 	 * Pause the excution this many milliseconds
 	 * 
