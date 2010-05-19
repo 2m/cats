d60631e95181501fe15a009d871a32714d964d12
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-19 14:37:07 +0000

    Import of landmark data and arena data from GSim

---
+++ trunk/cats/src/se/uu/it/cats/brick/filter/LandmarkList.java
@@ -0,0 +1,19 @@
+package se.uu.it.cats.brick.filter;
+
+public class LandmarkList {
+
+	public static final int GREEN = 1;
+	public static final int RED = 2;
+
+	// Positions of landmarks
+	public static final float[] landmarkX = { (float) 0.05, (float) 0.05,
+			(float) 2.95, (float) 2.95 };
+	public static final float[] landmarkY = { (float) 0.05, (float) 2.95,
+			(float) 0.05, (float) 2.95 };
+	public static final boolean[] landmarkC = { true, true, true, false };
+	// True if green, false if red
+
+	public LandmarkList() {
+	}
+
+}
