3976f039bb7dac26be256a75fda2d1e383c6d144
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-19 10:54:41 +0000

    Small changes to fix particle filter compability

--- cats/src/se/uu/it/cats/brick/TachoFeed.java
+++ cats/src/se/uu/it/cats/brick/TachoFeed.java
@@ -1,13 +1,14 @@
 package se.uu.it.cats.brick;
 
+import se.uu.it.cats.brick.filter.MovementData;
 import lejos.robotics.navigation.Pilot;
 
 /* @Nils Tornblom
  * For the real implementation of the absolute positioning particle filter.
  */
 public class TachoFeed {
-	/*
-	private RealTimeClock realtime;
+	
+	
 	public Buffer MovementBuffer;
 	private static Pilot pilot;
 	public static float lastR;
@@ -23,12 +24,12 @@ public class TachoFeed {
 	public static void pushMovement() {
 		newR = pilot.getTravelDistance();
 		newAngle = (float) (pilot.getAngle() * Math.PI/180f);
-		MovementData MD = new MovementData(realtime.getTime(), newR - lastR, newAngle - lastAngle);
+		MovementData MD = new MovementData(Clock.timestamp(), newR - lastR, newAngle - lastAngle);
 		MovementBuffer.push(MD);
 		lastR = newR;
 		lastAngle = newAngle;
 		
 	}
-	*/
+	
 		
 }
