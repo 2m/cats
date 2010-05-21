43ec9f50df8a46713652c8f11cae5da7c308b923
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-21 18:18:30 +0000

    by MMartynas, minor bug fixes

--- cats/src/se/uu/it/cats/brick/filter/MovementData.java
+++ cats/src/se/uu/it/cats/brick/filter/MovementData.java
@@ -2,9 +2,7 @@ package se.uu.it.cats.brick.filter;
 
 public class MovementData extends ComparableData {
 	public float dr; // Distance travelled
-	public float dangle; // Radians turned
-	public double vx;  //speed in m/s
-	public double vy;
+	public float dangle; // Radians turned	
 
 	public MovementData(int timestamp, float dr, float dangle) {
 		super(timestamp);
@@ -12,17 +10,11 @@ public class MovementData extends ComparableData {
 		this.dangle = dangle;
 	}
 
-	public MovementData(int timestamp, double xv, double yv) {
-		super(timestamp);
-		this.vx = xv;
-		this.vy = yv;
-	}
-
 	public boolean isMovementData() {
 		return true;
 	}
 
 	public String toString() {
-		return "[" + comparable + ",dr= " + dr + ",dangle= " + dangle + ",vx= " + vx + ",vy= " + vy + "]";
+		return "[" + comparable + ",dr= " + dr + ",dangle= " + dangle + "]";
 	}
 }
