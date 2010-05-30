a14cae5b2013e082ff5702f3b85cc8d24aa2b09d
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-05-30 13:04:11 +0000

    Edvard: Tweaked some settings in abs.pos.ukf. Abs.pos.ukf together with tracking.particlefilter seems to work ok @ 400ms each.

--- simulation/particle_filter/GSim/src/GSim/Cat.java
+++ simulation/particle_filter/GSim/src/GSim/Cat.java
@@ -17,16 +17,16 @@ public class Cat extends Actor {
 
 	private boolean usePositioningParticleFilter = false;
 	private boolean usePositioningUnscentedKalmanFilter = true;
-	private boolean useTrackingParticleFilter = false;
+	private boolean useTrackingParticleFilter = true;
 	private boolean useTrackingUnscentedKalmanFilter = false;
 	private boolean useGuide = false;
 
 	/* Periods in ms */
-	private int positioningNaivePeriod = 200;//200;
+	private int positioningNaivePeriod = 500;//200;
 	private int trackingKalmanPeriod = 200;//50;
-	private int positioningKalmanPeriod = 200;//200;
+	private int positioningKalmanPeriod = 400;//500; //NB: working ok @500ms with q = 0.5f and stddegrees = 1f, working somewhat ok @750ms with q = 0.005f and stddegrees = 1f
 	private int trackingParticlePeriod = 200;
-	private int positioningParticlePeriod = 200;
+	private int positioningParticlePeriod = 400;
 
 	private int nexttrack, nextpos;
 	private int Ntracking = 100;
