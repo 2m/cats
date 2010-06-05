b8d00608252a2ac065dd5c1364fd076fabc187ca
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-06-05 15:26:48 +0000

    by MMartynas, added Mean and Covariance view to the GUI.

--- trunk/cats-pc/src/se/uu/it/cats/pc/gui/BillBoardPanel.java
+++ trunk/cats-pc/src/se/uu/it/cats/pc/gui/BillBoardPanel.java
@@ -26,6 +26,7 @@ public class BillBoardPanel extends JPanel
 {
 	private LatestSighting[] _latestSightings = new LatestSighting[3];
 	private AbsolutePosition[] _absolutePositions = new AbsolutePosition[3];
+	private MeanAndCovariance[] _meanAndCovariance = new MeanAndCovariance[3];
 	
 	public BillBoardPanel()
 	{
@@ -46,6 +47,14 @@ public class BillBoardPanel extends JPanel
 			panel.add(_absolutePositions[i]);			
 		}
 		add(panel, BorderLayout.CENTER);
+		
+		panel = new JPanel();
+		for (int i = 0; i < Area.CAT_COUNT; i++)
+		{
+			_meanAndCovariance[i] = new MeanAndCovariance(i);
+			panel.add(_meanAndCovariance[i]);			
+		}
+		add(panel, BorderLayout.SOUTH);
 	}
 	
 	public void repaint()
@@ -59,6 +68,9 @@ public class BillBoardPanel extends JPanel
 			
 			for (AbsolutePosition ap: _absolutePositions)
 				ap.repaint();
+			
+			for (MeanAndCovariance mac: _meanAndCovariance)
+				mac.repaint();
 		}
 		catch (NullPointerException ex) {}
 	}
@@ -145,8 +157,7 @@ public class BillBoardPanel extends JPanel
 				_timestamp.setText(String.format("%07.3f", BillBoard.getInstance().getLatestSightingTimestamp(_id)));
 			}
 			catch (NullPointerException ex) {}
-		}
-		
+		}		
 	}
 	
 	private class AbsolutePosition extends JPanel
@@ -231,7 +242,106 @@ public class BillBoardPanel extends JPanel
 				_timestamp.setText(String.format("%07.3f", BillBoard.getInstance().getAbsolutePositionTimestamp(_id)));
 			}
 			catch (NullPointerException ex) {}
+		}		
+	}
+	
+	private class MeanAndCovariance extends JPanel
+	{
+		private int _id;
+		
+		private JLabel _meanX, _meanY, _meanXv, _meanYv,
+		_varXX, _varXY, _varYY, _varXvXv, _varXvYv, _varYvYv, _weight, _timestamp;		
+		
+		public MeanAndCovariance(int id)
+		{
+			super(new BorderLayout());
+			
+			_id = id;
+			
+			// GridLayout(int rows, int cols, int hgap, int vgap)
+			JPanel panel = new JPanel(new GridLayout(12, 2, 10, 0));
+			
+			setBorder(new TitledBorder(
+					new LineBorder(Color.gray, 1, false),
+					"cat"+ _id + " MeanAndCov",
+					TitledBorder.LEFT,
+					TitledBorder.DEFAULT_POSITION)
+			);
+			
+			panel.add(new JLabel("Mean X"));
+			_meanX = new JLabel("1");
+			panel.add(_meanX);
+			
+			panel.add(new JLabel("Mean Y"));
+			_meanY = new JLabel("2");
+			panel.add(_meanY);
+			
+			panel.add(new JLabel("Mean Xv"));
+			_meanXv = new JLabel("3");
+			panel.add(_meanXv);
+			
+			panel.add(new JLabel("Mean Yv"));
+			_meanYv = new JLabel("4");
+			panel.add(_meanYv);
+			
+			panel.add(new JLabel("Var XX"));
+			_varXX = new JLabel("4");
+			panel.add(_varXX);
+			
+			panel.add(new JLabel("Var XY"));
+			_varXY = new JLabel("4");
+			panel.add(_varXY);
+			
+			panel.add(new JLabel("Var YY"));
+			_varYY = new JLabel("4");
+			panel.add(_varYY);
+			
+			panel.add(new JLabel("Var XvXv"));
+			_varXvXv = new JLabel("4");
+			panel.add(_varXvXv);
+			
+			panel.add(new JLabel("Var XvYv"));
+			_varXvYv = new JLabel("4");
+			panel.add(_varXvYv);
+			
+			panel.add(new JLabel("Var YvYv"));
+			_varYvYv = new JLabel("4");
+			panel.add(_varYvYv);
+			
+			panel.add(new JLabel("Weight"));
+			_weight = new JLabel("4");
+			panel.add(_weight);
+			
+			panel.add(new JLabel("Timestamp"));
+			_timestamp = new JLabel("4");
+			panel.add(_timestamp);
+			
+			add(panel, BorderLayout.NORTH);
 		}
 		
+		public void repaint()
+		{
+			try
+			{
+				super.repaint();
+				
+				_meanX.setText(String.format("%07.3f", BillBoard.getInstance().getMeanX(_id)));
+				_meanY.setText(String.format("%07.3f", BillBoard.getInstance().getMeanY(_id)));
+				_meanXv.setText(String.format("%07.3f", BillBoard.getInstance().getMeanXv(_id)));
+				_meanYv.setText(String.format("%07.3f", BillBoard.getInstance().getMeanYv(_id)));
+				
+				_varXX.setText(String.format("%07.3f", BillBoard.getInstance().getVarXX(_id)));
+				_varXY.setText(String.format("%07.3f", BillBoard.getInstance().getVarXY(_id)));
+				_varYY.setText(String.format("%07.3f", BillBoard.getInstance().getVarYY(_id)));
+				
+				_varXvXv.setText(String.format("%07.3f", BillBoard.getInstance().getVarXvXv(_id)));
+				_varXvYv.setText(String.format("%07.3f", BillBoard.getInstance().getVarXvYv(_id)));
+				_varYvYv.setText(String.format("%07.3f", BillBoard.getInstance().getVarYvYv(_id)));
+				
+				_weight.setText(String.format("%07.3f", BillBoard.getInstance().getWeight(_id)));
+				_timestamp.setText(String.format("%07.3f", BillBoard.getInstance().getDataTimestamp(_id)));
+			}
+			catch (NullPointerException ex) {}
+		}		
 	}
 }
