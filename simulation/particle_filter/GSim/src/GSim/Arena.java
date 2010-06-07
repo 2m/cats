c8d8e8c25d118455e3364feef4c7926622950b0d
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-06-07 10:28:24 +0000

    Depricated

--- simulation/particle_filter/GSim/src/GSim/Arena.java
+++ simulation/particle_filter/GSim/src/GSim/Arena.java
@@ -2,10 +2,11 @@ package GSim;
 
 /** Static data with data on arena properties */
 public class Arena {
-	public static final float min_x = 0;
-	public static final float max_x = (float) 3;
-	public static final float min_y = 0;
-	public static final float max_y = (float) 3;
+	// FIXME: Depricated
+	public static final float min_x_ = 0;
+	public static final float max_x_ = (float) 3;
+	public static final float min_y_ = 0;
+	public static final float max_y_ = (float) 3;
 
 	public Arena() {
 	}
--- simulation/particle_filter/GSim/src/GSim/LandmarkList.java
+++ simulation/particle_filter/GSim/src/GSim/LandmarkList.java
@@ -1,17 +1,17 @@
 package GSim;
 
 public class LandmarkList {
-	// FIXME: Add real colours to landmark lists
-	public static final int MOUSE = 0;
-	public static final int GREEN = 1;
-	public static final int RED = 2;
+	// FIXME: Depricated
+	public static final int MOUSE_ = 0;
+	public static final int GREEN_ = 1;
+	public static final int RED_ = 2;
 
 	// Positions of landmarks
-	public static final float[] landmarkX = { (float) 0.05, (float) 0.05,
+	public static final float[] landmarkX_ = { (float) 0.05, (float) 0.05,
 			(float) 2.95, (float) 2.95 };
-	public static final float[] landmarkY = { (float) 0.05, (float) 2.95,
+	public static final float[] landmarkY_ = { (float) 0.05, (float) 2.95,
 			(float) 0.05, (float) 2.95 };
-	public static final boolean[] landmarkC = { true, true, true, false };
+	public static final boolean[] landmarkC_ = { true, true, true, false };
 	// True if green, false if red
-	public static final int[] Colour = { GREEN, GREEN, GREEN, RED };
+	public static final int[] Colour_ = { GREEN, GREEN, GREEN, RED };
 }
