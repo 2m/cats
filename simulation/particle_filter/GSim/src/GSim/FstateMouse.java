7cf0416f7739e94a109afba528c14b3a0c666bd9
jf.wahlberg <jf.wahlberg@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-06-08 11:22:11 +0000

    by Edvard: trying to fix strange connection for a few files between brick and GSim

---
+++ simulation/particle_filter/GSim/src/GSim/FstateMouse.java
@@ -0,0 +1,34 @@
+package GSim;
+
+import lejos.util.Matrix;
+
+/**
+ * State function for the (tracked) mouse's position
+ * @author Edvard
+ */
+public class FstateMouse implements IFunction{
+	
+	/** Period of filter */
+	protected final float dt;
+	
+	public FstateMouse(float dt)
+	{
+		this.dt = dt;
+	}
+	
+	public Matrix eval(Matrix xm){
+			
+		Matrix xm_updated = Matlab.zeros(xm.getRowDimension(), xm.getColumnDimension());
+		if (xm.getRowDimension() != 4|| xm.getColumnDimension() != 1)
+		{
+		System.out.println("WARNING, update FstateMouse to match the state vector");
+		}
+		Matrix F = Matlab.eye(4);
+		//here dt=1 since speed is measured in m/samples, thus:
+		F.set(1-1,3-1, dt);
+		F.set(2-1,4-1, dt);	
+		xm_updated=F.times(xm);
+		return xm_updated; 
+	}
+}
+
