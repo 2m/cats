38e29134c7ddcf1abab4ad983540779ae60435ed
nils.tornblom <nils.tornblom@0d9175de-1193-11df-992a-f3fa5211fe9f>
2010-08-17 21:34:47 +0000

    minor changes to convertAllFigs for increased robustness

--- trunk/reports/final_report/Matlab_export_fig_to_PDF/convertAllFigs.m
+++ trunk/reports/final_report/Matlab_export_fig_to_PDF/convertAllFigs.m
@@ -1,12 +1,16 @@
+%Default width and height of the figure to be saved. The size of the figure
+%determines the relative size of the graphics such as axis names.
+Wx=715;
+Wy=83;
 A=dir;
-mkdir('PDF');
+mkdir('PDFs');
 for i=[3:size(A,1)]
-    open(A(i,:).name);
     if (A(i,:).name(end-3:end) == '.fig');
+        open(A(i,:).name);
         nameCut = A(i,:).name(1:end-4);
         set(gcf,'WindowStyle','modal')
-        set(gcf,'Position',[10 800 725 533]);
-        cd PDF
+        set(gcf,'Position',[10 450 10+Wx 450+Wy]);
+        cd PDFs
         savefig(strcat(nameCut,'.pdf'),'pdf');
         cd ..
     else
